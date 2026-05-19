cb9b67772beb0b83361ad94613619cd77d89099a
abrahamBM20 <abr.barrera@duocuc.cl>
2026-05-19 14:40:35 -0400

    funciona

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de las compras 💰"
+          title="Consultar Ordenes de compras 💰"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
