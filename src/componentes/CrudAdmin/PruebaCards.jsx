dea978ae206aca125a43a94025ad5c93016a29bf
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-25 16:58:01 -0400

    deploy

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compras compras 💰"
+          title="Consultar Ordenes de compraaas compras 💰"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
